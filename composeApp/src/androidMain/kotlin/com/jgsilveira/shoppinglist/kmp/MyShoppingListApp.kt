359b7dee48ef385e23418cf651158345bef418af
jonathan-silveira <jonathangsilveira@gmail.com>
2025-02-23 18:03:39 -0300

    Apply koin initialization to android app

--- composeApp/src/androidMain/kotlin/com/jgsilveira/shoppinglist/kmp/MyShoppingListApp.kt
+++ composeApp/src/androidMain/kotlin/com/jgsilveira/shoppinglist/kmp/MyShoppingListApp.kt
@@ -2,14 +2,19 @@ package com.jgsilveira.shoppinglist.kmp
 
 import android.app.Application
 import com.jgsilveira.shoppinglist.kmp.counter.counterKoinModule
+import com.jgsilveira.shoppinglist.kmp.di.initKoin
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 internal class MyShoppingListApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            modules(counterKoinModule)
-        }
+        initKoin(
+            definition = {
+                androidContext(this@MyShoppingListApp)
+            }
+        )
     }
 }
