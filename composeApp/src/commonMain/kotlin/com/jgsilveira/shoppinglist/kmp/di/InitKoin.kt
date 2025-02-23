57c33d7fdb8d7f75536ee2ba97d0bb6e7e831b78
jonathan-silveira <jonathangsilveira@gmail.com>
2025-02-23 18:03:14 -0300

    Add additional modules parameter to koin initialization

--- composeApp/src/commonMain/kotlin/com/jgsilveira/shoppinglist/kmp/di/InitKoin.kt
+++ composeApp/src/commonMain/kotlin/com/jgsilveira/shoppinglist/kmp/di/InitKoin.kt
@@ -1,12 +1,17 @@
 package com.jgsilveira.shoppinglist.kmp.di
 
 import com.jgsilveira.shoppinglist.kmp.counter.counterKoinModule
+import com.jgsilveira.shoppinglist.kmp.preferences.platformKoinModule
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
 
-fun initKoin(config: KoinAppDeclaration? = null) {
+fun initKoin(
+    definition: KoinAppDeclaration = {},
+    additionalModules: List<Module> = emptyList()
+) {
     startKoin {
-        config?.invoke(this)
-        modules(counterKoinModule)
+        definition.invoke(this)
+        modules(additionalModules + platformKoinModule + counterKoinModule)
     }
 }
